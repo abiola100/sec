b2f75913bfbc12ad181d24ba7d9ef5b677df325a
abiola100 <abiolabusari827@gmail.com>
2025-06-26 19:06:20 +0100

    second commit

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,47 @@ const CACHE_NAME = 'secondary-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/icon-192.png',
-  '/icon-512.png',
-  // Add more assets here if you want to cache them
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/manifest.json',
+  '/terms/first-term/agric-first/agric-first.html',
+  '/terms/second-term/agric-second/agric-second.html',
+  '/terms/third-term/agric-third/agric-third.html',
+  '/terms/first-term/agric-first/first-term-js2-agric.html',
+  '/terms/second-term/agric-second/second-term-js2-agric.html',
+  '/terms/third-term/agric-third/third-term-js3-agric.html',
+  '/terms/first-term/agric-first/first-term-js3-agric.html',
+  '/terms/second-term/agric-second/second-term-js3-agric.html',
+  '/terms/third-term/agric-third/third-term-js3-agric.html',
+
+  '/style.css',
+  '/style2.css',
+  '/script.js',
+  '/script2.js'
+  // Add CSS, JS, fonts, etc., if applicable
 ];
+
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate: optional cleanup for old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
   );
 });
 
